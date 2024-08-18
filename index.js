7760292b105ab06f4ecba414b8e1d3f19e98eb20
CaoMeiYouRen <996881204@qq.com>
2024-08-19 00:40:29 +0800

    feat: 新增 当标题中没有日期时，自动增加日期

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
 
 const fs = require('fs-extra');
 const Parser = require('rss-parser');
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
 
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+const dateReg = /^\d{4}-\d{2}-\d{2}\s+/
 const rssParser = new Parser();
+
 async function start() {
     const blogResp = await rssParser.parseURL('https://blog.cmyr.ltd/atom.xml');
     if (Date.now() - new Date(blogResp.lastBuildDate).getTime() > 48 * 60 * 60 * 1000) {
@@ -11,7 +19,12 @@ async function start() {
         return;
     }
     const items = [...blogResp.items.slice(0, 5)];
-    const text = items.map((e) => `- [${e.title}](${e.link})`).join('\n');
+    const text = items.map((e) => {
+        if (dateReg.test(e.title)) { // 如果标题中有日期
+            return `- [${e.title}](${e.link})`
+        }
+        return `- [${dayjs.tz(e.pubDate, 'Asia/Shanghai').format('YYYY-MM-DD')} ${e.title}](${e.link})`
+    }).join('\n');
     const readme = await fs.readFile('README.md', 'utf-8');
     const newReadme = readme.replace(/<!-- BLOG_START -->([\s\S]*?)<!-- BLOG_END -->/, `<!-- BLOG_START -->\n${text}\n<!-- BLOG_END -->`);
     await fs.writeFile('README.md', newReadme);
